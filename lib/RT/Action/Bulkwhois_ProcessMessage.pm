49f911b733d5b84c33ab0e6ce5c7f2e78da7a203
Wes Young <github@barely3am.com>
2012-02-03 12:35:33 -0500

    adding more files

---
+++ lib/RT/Action/Bulkwhois_ProcessMessage.pm
@@ -0,0 +1,12 @@
+package RT::Action::Bulkwhois_ProcessMessage;
+
+use strict;
+use warnings;
+
+use base qw(RT::Action::Generic);
+
+sub Prepare { }
+
+sub Commit {}
+
+1;
---
+++ lib/RT/Condition/Bulkwhois.pm
@@ -0,0 +1,10 @@
+package RT::Condition::Bulkwhois;
+
+use strict;
+use warnings;
+
+use base 'RT::Condition::Generic';
+
+sub IsBulkWhois { }
+
+1;
